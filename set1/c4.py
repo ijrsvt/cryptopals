9654d7ad816c1dc676dbe3422d6b2abb19523c0b
Ian Rodney <ian.rodney@gmail.com>
2021-09-10 00:03:21 -0700

    set c4

---
+++ set1/c4.py
@@ -0,0 +1,9 @@
+from itertools import chain
+from typing import List
+
+from .c3 import *
+
+
+def try_all_inputs(file_name: str) -> List[bytes]:
+    inputs = open(file_name).readlines()
+    return list(chain(*[try_all_xor(f) for f in inputs]))
