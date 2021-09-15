34b6ddd5ee98b63f670a1ddb821f7b1f60a6cc22
Ian Rodney <ian.rodney@gmail.com>
2021-09-15 00:06:33 -0700

    c7

---
+++ set1/c7.py
@@ -0,0 +1,7 @@
+from Crypto.Cipher import AES
+
+
+def decrypt_aes_128(msg: bytes, key: bytes) -> bytes:
+    assert len(key) == 16, "AES 128 requires a 16 Byte Key!"
+    cipher = AES.new(key, AES.MODE_ECB)
+    return cipher.decrypt(msg)
