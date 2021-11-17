2870d3907cc530c6018ea2cb744503d61b8d6dc1
Ian Rodney <ilr@anyscale.com>
2021-11-17 14:04:25 -0500

    hacked

--- set2/c13.py
+++ set2/c13.py
@@ -1,3 +1,4 @@
+import math
 import secrets  
 from typing import Callable, Dict
 
@@ -39,13 +40,17 @@ class Oracle:
     def encrypt(self, email: str) -> bytes:
         return self._inner_encrypt(profile_for(email).encode())
 
+
+    def _inner_decrypt(self, b: bytes) -> bytes:
+        return decrypt_aes_128_ecb(b, self.key)
+
     def decode(self, encrypted_bytes: bytes) -> Dict[str,str]:
-        return parse_url(decrypt_aes_128_ecb(encrypted_bytes, self.key).decode())
+        return parse_url(self._inner_decrypt(encrypted_bytes).decode())
 
 
 
 def generate_encrypted_bytes_for_admin(encrypt_fn: Callable[[str], bytes]):
-    desired_string = "role=admin"
+    desired_string = "admin"
     pad_size = determine_block_size(lambda x: encrypt_fn(x.decode()))
     padded = pad_bytes(desired_string.encode(), pad_size).decode()
 
@@ -54,17 +59,51 @@ def generate_encrypted_bytes_for_admin(encrypt_fn: Callable[[str], bytes]):
 
     return encrypt_fn(initial_block + padded)[pad_size:2*pad_size]
 
+def determine_empty_len_size(o: Callable[[str], bytes]):
+    original_size = len(o(""))
+    i = 1
+    while True:
+        new_size = len(o("X" * i))
+        if new_size > original_size:
+            return original_size - i
+        i += 1
 
-def solve():
+def test_determine_empty_len_size():
+    def fn(desired_size: int):
+        def inner_fn(s: str):
+            og = "X" * desired_size
+            (og + s).encode()
+            return encrypt_aes_128_ecb((og + s).encode(), secrets.token_bytes(16))
+        return inner_fn
+    for i in range(16):
+        size =  determine_empty_len_size(fn(i))
+        assert size == i, f"Got {size}, not {i}"
+
+
+
+def solve(oracle: Callable[[str], bytes]) -> bytes:
     """
-    1. Modify the input to get a single AES Encrypted 16B block that begins with `role=admin` & properly padded
-    2. Generate an input that sets `role=user` onto its own block
+    1. Modify the input to get a single AES Encrypted 16B block that begins with `admin` & properly padded
+    2. Generate an input that sets `user` onto its own block
     3. Swap the last block with the block that you created in part 1
     4. Decrypt & Profit
     """
+    admin_block = generate_encrypted_bytes_for_admin(oracle)
+    original_size = determine_empty_len_size(oracle)
+    pad_size = determine_block_size(lambda x: oracle(x.decode()))
+    email_bytes = (math.ceil(original_size * 1.0 / pad_size))
+    bs_bytes = oracle("A" * (email_bytes * pad_size - original_size + + len("user")))
+    return bs_bytes, bs_bytes[:-16] + admin_block
+    
 
 if __name__ == "__main__":
     o =Oracle()
     ENCRYPTED_USER = o.encrypt("ian.rodney@gmail.com") 
     DECODED = o.decode(ENCRYPTED_USER)
     print("Encrypted", ENCRYPTED_USER, "\n", "Decrypted", DECODED)
+    test_determine_empty_len_size()
+    potential = generate_encrypted_bytes_for_admin(o.encrypt)
+
+    normal, hacked = solve(o.encrypt)
+    assert o.decode(hacked)["role"] == "admin", o.decode(hacked)
+    print(f"\nWe now have this role: {o.decode(hacked)}")
